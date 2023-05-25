191b0c45dbe19f5aecafafe7235bf15b492fdbd4
Your Name <you@example.com>
2023-05-25 18:12:24 +0300

    lopetus kerta

--- 2023kevatTo1715_unity_harjoitus/Assets/Scripts/PlayerController.cs
+++ 2023kevatTo1715_unity_harjoitus/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public CharacterController characterController;
     public float moveSpeed = 5;
     public float mouseSpeed = 5;
+    public float maxAngle = 70f;
+    public float minAngle = -70f;
     private float mouseVertical = 0;
     private float mouseHorizontal = 0;
 
@@ -20,12 +22,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         mouseHorizontal += Input.GetAxis("Mouse X") * mouseSpeed;
-        mouseVertical += Input.GetAxis("Mouse Y") * mouseSpeed;
-        Camera.main.transform.localRotation = Quaternion.Euler(mouseHorizontal, mouseVertical,0);
+        mouseVertical -= Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        mouseVertical = Mathf.Clamp(mouseVertical,minAngle,maxAngle);
+
+        Camera.main.transform.localRotation = Quaternion.Euler( mouseVertical, mouseHorizontal,0);
+
         float forwardMove = Input.GetAxis("Vertical");
         float sideMove = Input.GetAxis("Horizontal");
 
         Vector3 direction = new Vector3(sideMove,0,forwardMove);
+
+        direction = Camera.main.transform.rotation * direction;
+
         characterController.Move(direction * Time.deltaTime * moveSpeed);
     }
 }
